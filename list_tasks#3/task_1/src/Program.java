d7d73e33724ae39411a46e21aa5bd0e11cf4bef3
Artem <artyom543211@gmail.com>
2018-10-08 10:53:31 +0300

    clear code

--- list_tasks#3/task_1/src/Program.java
+++ list_tasks#3/task_1/src/Program.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) { 		
-		FormatText ft = new FormatText();
 		Scanner sc = new Scanner(System.in);
-		for (String string : ft.getWords(sc.nextLine())) {
+		for (String string : sc.nextLine().split(" ")) {
 			System.out.println(string);			
 		}
 	}
